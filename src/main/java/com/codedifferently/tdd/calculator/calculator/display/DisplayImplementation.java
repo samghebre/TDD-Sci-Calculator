fe38bb646496ed5ddbdbfb13fa1ebad5f446ca6d
stainlessray <raymondcoolmail@gmail.com>
2020-11-20 19:33:12 -0500

    cleaned up

--- src/main/java/com/codedifferently/tdd/calculator/calculator/display/DisplayImplementation.java
+++ src/main/java/com/codedifferently/tdd/calculator/calculator/display/DisplayImplementation.java
@@ -1,6 +1,5 @@
 package com.codedifferently.tdd.calculator.calculator.display;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class DisplayImplementation implements Display{
@@ -14,21 +13,19 @@ public class DisplayImplementation implements Display{
     @Override
     public DisplayMode switchDisplayMode() {
         int newDisplayOrdinal;
-        if (getCurrentDisplayMode().ordinal() == DisplayMode.values().length-1) {
-            newDisplayOrdinal = 0;
-        } else newDisplayOrdinal = getCurrentDisplayMode().ordinal()+1;
+        if (getCurrentDisplayMode().ordinal() == DisplayMode.values().length-1)
+            { newDisplayOrdinal = 0;
+            } else newDisplayOrdinal = getCurrentDisplayMode().ordinal()+1;
+
         String valueString = Arrays.toString(DisplayMode.values());
         String[] options = valueString.split(" ");
         String newMode  = options[newDisplayOrdinal].replaceAll("]", "").trim();
-        this.displayMode = DisplayMode.valueOf(newMode);
-        return this.displayMode;
+        return this.displayMode = DisplayMode.valueOf(newMode);
     }
 
     @Override
     public DisplayMode switchDisplayMode(String mode) {
-        this.displayMode = DisplayMode.valueOf(mode.toUpperCase());
-        return this.displayMode;
-        //todo input validation restraining to valid options
+        return this.displayMode = DisplayMode.valueOf(mode.toUpperCase());
     }
 
     @Override
